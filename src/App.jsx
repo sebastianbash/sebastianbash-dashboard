cac2b034877290f2d8ea7e4c236462f9bb1f1567
sebastian <sebastian.silva.agurto@hotmail.com>
2022-08-04 15:33:09 -0500

    REST API a short test with frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  const [isLogged, setIsLogged] = useState(false);
+  // if the api return data, save it to a state.
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await axios.post(import.meta.env.VITE_API_URL, {
+          email: "prueba1@gmail.com",
+          password: "prueba123",
+        });
+        res.data.token ? setIsLogged(true) : setIsLogged(false);
+      } catch (error) {
+        setIsLogged(false);
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
 
   return (
     <div>
       <h1>Hello World!!!</h1>
+      {
+        isLogged ? <p className="font-bold">Bienvenido Usuario: Pepito</p> : null
+      }
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
