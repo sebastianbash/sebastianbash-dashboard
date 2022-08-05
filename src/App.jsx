36e9307e2ad73f5d258662e990d5719f4f382e94
sebastian <sebastian.silva.agurto@hotmail.com>
2022-08-05 16:07:42 -0500

    Route login and dashboard implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
 import React from 'react';
-// import axios from "axios";
+import { Route, Routes } from 'react-router-dom';
+import Protected from './components/protectedRoute/Protected';
+import Validated from './components/validatedRoute/Validated';
+import Dashboard from './components/views/dashboard/Dashboard';
+import Login from './components/views/login/Login';
 
 function App() {
-  // const [isLogged, setIsLogged] = useState(false);
-  // // if the api return data, save it to a state.
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     try {
-  //       const res = await axios.post(import.meta.env.VITE_API_URL, {
-  //         email: "prueba1@gmail.com",
-  //         password: "prueba123",
-  //       });
-  //       res.data.token ? setIsLogged(true) : setIsLogged(false);
-  //     } catch (error) {
-  //       setIsLogged(false);
-  //       console.log(error);
-  //     }
-  //   }
-  //   fetchData();
-  // }, []);
-
-  // return (
-  //   <div>
-  //     <h1>Hello World!!!</h1>
-  //     {
-  //       isLogged ? <p className="font-bold">Bienvenido Usuario: Pepito</p> : null
-  //     }
-  //   </div>
-  // );
-
-  return <h1>Hello world!</h1>;
+  return (
+    <Routes>
+      <Route path="/login" element={
+          <Login />
+      } /> 
+      <Route path="/dashboard" element={
+        <Protected>
+          <Dashboard />
+        </Protected>
+      } />
+      <Route path="*" element={<h2>Page not found 404</h2>} />
+    </Routes>
+  );
 }
 
 export default App;
