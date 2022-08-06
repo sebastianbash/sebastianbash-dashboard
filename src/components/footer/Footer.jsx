98b29adb2e3a00347b2d649d4737a2f9e3983b84
sebastian <sebastian.silva.agurto@hotmail.com>
2022-08-06 17:44:05 -0500

    Footer added

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className="mt-auto w-full md:h-[10%] flex flex-col justify-center items-center text-gray-500 dark:text-gray-300">{new Date().getFullYear()} &copy; | SebastianBash All Rights Reserved.</div>
+  )
+}
