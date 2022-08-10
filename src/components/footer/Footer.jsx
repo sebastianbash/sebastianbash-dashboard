eecdd757ddda38e33e1a8ba56e0d7c9e40520609
sebastian <sebastian.silva.agurto@hotmail.com>
2022-08-10 13:49:26 -0500

    Footer fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="mt-auto w-full md:h-[10%] flex flex-col justify-center items-center text-gray-500 dark:text-gray-300">{new Date().getFullYear()} &copy; | SebastianBash All Rights Reserved.</div>
+    <div className="mt-auto w-full md:h-[10%] flex flex-col justify-center items-center text-gray-500 dark:text-gray-300">Copyright &copy; {new Date().getFullYear()} | SebastianBash All rights reserved.</div>
   )
 }
