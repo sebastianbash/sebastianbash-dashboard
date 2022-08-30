ead682f7f21aea073e0a7b05f9bcbbd9671e0e2e
sebastian <sebastian.silva.agurto@hotmail.com>
2022-08-29 22:11:52 -0500

    Footer component improved

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="mt-auto w-full md:h-[10%] flex flex-col justify-center items-center text-gray-500 dark:text-gray-300">Copyright &copy; {new Date().getFullYear()} | SebastianBash All rights reserved.</div>
+    <div className="">
+      <div className="brand-logo">
+        {/* TODO: reemplazar la imagen por el logo de la empresa */}
+        <a href="#" target="_blank" className="text-2xl font-bold">
+          <img width="80" height="80" src="https://links.papareact.com/gzs" alt="Trabajemos juntos" /> 
+        </a>
+      </div>
+      <div className="links">
+        <span>
+          &copy; {new Date().getFullYear()} <a href="#" targer="_blank" className="text-red-700">Mapogo Media</a>. All Rights Reserved
+        </span>
+      </div>
+    </div>
   )
 }
