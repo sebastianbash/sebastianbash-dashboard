dbb6044fcdb40a406552591e883508525f5bef17
sebastian <sebastian.silva.agurto@hotmail.com>
2022-08-29 22:10:31 -0500

    Signup route addded

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import Protected from './components/protectedRoute/Protected';
 import Validated from './components/validatedRoute/Validated';
 import Dashboard from './components/views/dashboard/Dashboard';
 import Login from './components/views/login/Login';
+import Signup from './components/views/signup/Signup';
+
 
 function App() {
   return (
     <Routes>
       <Route index element={<Navigate to="/login" replace={true} />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route
         path="/dashboard"
         element={
