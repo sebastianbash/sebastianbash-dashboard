f860ebb4abe0c7dc8decbcc009d6854626b38789
sebastian <sebastian.silva.agurto@hotmail.com>
2022-08-29 22:14:45 -0500

    Structure improved

--- src/components/views/dashboard/Layout.jsx
+++ src/components/views/dashboard/Layout.jsx
@@ -4,12 +4,15 @@ import Footer from '../../footer/Footer';
 
 export default function Layout({ children }) {
   return (
-    <div className="layout-dashboard w-full flex flex-col lg:block min-h-screen h-screen p-4 md:p-8">
-      <div className="flex flex-col mb-10 lg:flex-row lg:justify-center lg:h-[90%]">
+    // <div className="layout-dashboard w-full min-h-screen h-full flex flex-col lg:block p-4 md:p-8">
+    <div className="layout-dashboard w-full min-h-screen h-full grid grid-cols-12 grid-rows-6 p-4 md:p-8">
+      <div className="header col-span-4 row-span-6 flex flex-col justify-between">
         <Header />
+        <Footer />
+      </div>
+      <div className="main col-span-8 row-span-6">
         {children}
       </div>
-      <Footer />
     </div>
   )
 }
