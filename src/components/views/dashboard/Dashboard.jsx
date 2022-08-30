4def69208833be47b57e5f5afb4a3593dc587242
sebastian <sebastian.silva.agurto@hotmail.com>
2022-08-29 22:17:24 -0500

    simple commit

--- src/components/views/dashboard/Dashboard.jsx
+++ src/components/views/dashboard/Dashboard.jsx
@@ -1,28 +1,17 @@
-import React, { useEffect, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import Layout from './Layout';
+import Project from '../../project/Project';
+import Service from '../../service/Service';
 
 export default function Dashboard() {
-  let navigate = useNavigate();
-  const userLogout = useRef();
-  const logOut = () => {
-    window.localStorage.removeItem('user');
-    navigate('/login', { replace: true });
-  };
-
   return (
     <Layout>
-      <div className="main-side w-full lg:w-3/4 bg-gray-300 rounded-[20px] mt-8 px-4 py-2 md:mt-0 md:px-10 md:py-6">
-        <div className="">Dashboard</div>
-        <button
-          ref={userLogout}
-          type="button"
-          onClick={logOut}
-          className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-        >
-          Log out
-        </button>
-      </div>
+      {/* Projects */}
+      {/* <Project /> */}
+      {/* Services */}
+      <Service />
+      {/* Contact */}
+      {/* Account */}
     </Layout>
   );
 }
